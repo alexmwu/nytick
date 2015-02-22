9af30da419ad8e1f2c1b083bf4132f1379687171
Chathura Colombage <dcdewaka@gmail.com>
2015-02-22 12:50:21 +0000

    return stock info

--- nytick.py
+++ nytick.py
@@ -104,17 +104,14 @@ def index(name=None):
     for article in p_stories["results"]:
         info = {}
         symbols, pub_date = grab_tickers(article)
-        if pub_date == -1:
-            info['is_ticker'] = False
-            info['symbols'] = []
-        else:
+        if pub_date != -1:
             info['is_ticker'] = True
             info['symbols'] = symbols
-        info['data'] = article
-        articles.append(info)
+            info['data'] = article
+            info['stock'] = get_stock_data(symbols)
+            articles.append(info)
 
     return render_template('c3test.html', popular_stories=json.dumps(articles))
->>>>>>> c81ad7baea5226bd2be1ec15f44607c48624cd5a
 
 def main():
     # from bloomberg import ticks
